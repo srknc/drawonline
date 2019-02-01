b6dd714e8da77c7f03facdf5f2210dc58b60b23c
srknc <serkan.capkan@gmail.com>
2019-02-01 18:52:03 +0100

    new cloudformation(nested)

--- cloudformaion_deploy.sh
+++
@@ -1,1 +0,0 @@
-aws cloudformation update-stack --stack-name drawonline --template-body file://cloudformation.yml --parameters ParameterKey=RootDomainName,ParameterValue=drawonline.io ParameterKey=HostedZoneId,ParameterValue=ZE05893RK9IXZ
---
+++ cloudformation/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+
+bucket_administrative=cloudformation-1548978758
+hosted_zone_id=ZE05893RK9IXZ
+root_domain_name=drawonline.io
+
+
+# get current region
+region=$(aws configure list | grep region | awk {'print $2'})
+
+# upload templates
+for i in $(ls *.yaml)
+do
+  aws s3 cp $i s3://$bucket_administrative/ &
+done
+
+
+# update cloudformation
+aws cloudformation update-stack \
+  --stack-name drawonline \
+  --template-url https://s3.amazonaws.com/$bucket_administrative/root.yaml \
+  --parameters  ParameterKey=RootDomainName,ParameterValue=$root_domain_name \
+                ParameterKey=HostedZoneId,ParameterValue=$hosted_zone_id \
+                ParameterKey=BucketAdministrativeName,ParameterValue=$bucket_administrative \
+  --capabilities CAPABILITY_IAM
